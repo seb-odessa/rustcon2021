77236fcaffbcfd824a1c5e1cca22e98e4206daa6
seb <seb@ukr.net>
2022-03-13 12:26:23 +0200

    Problem G first working version

--- src/bin/problem-g.rs
+++ src/bin/problem-g.rs
@@ -4,13 +4,91 @@
  */
 
 use lib::read_line;
+use lib::SuffixArray;
+use std::collections::HashSet;
 
-struct Calculator {}
+struct Calculator {
+    suffixes: Vec<SuffixArray>,
+    patterns: HashSet<String>,
+}
 impl Calculator {
     pub fn new() -> Self {
-        return Self {};
+        return Self {
+            suffixes: Vec::new(),
+            patterns: HashSet::new(),
+        };
     }
-    pub fn add(name: &String) -> String {}
+    fn is_should_be_searched(name: String) -> bool {
+        return "+" == &name[name.len() - 1..];
+    }
+
+    fn get_search_patterns(&self) -> Vec<String> {
+        let mut set = HashSet::new();
+        for sa in self.suffixes.iter() {
+            if Self::is_should_be_searched(sa.get_text()) {
+                if set.is_empty() {
+                    set = sa.get_substrs();
+                } else {
+                    set = set.intersection(&sa.get_substrs()).cloned().collect();
+                }
+            }
+        }
+        let mut v = set.into_iter().collect::<Vec<String>>();
+        v.sort_by(|a, b| a.len().cmp(&b.len()).then(a.cmp(&b)));
+        return v;
+    }
+
+    pub fn add<T: Into<String>>(&mut self, name: T) -> String {
+        self.suffixes.push(SuffixArray::new(name.into()));
+        let mut sp = self.get_search_patterns();
+        for sa in self.suffixes.iter() {
+            if !Self::is_should_be_searched(sa.get_text()) {
+                let mut keep = Vec::new();
+                for pattern in sp {
+                    if sa.search(&pattern).is_none() {
+                        keep.push(pattern);
+                    }
+                }
+                sp = keep;
+            }
+        }
+        println!("sp: {:?}\n", sp);
+        if sp.is_empty() {
+            String::from("-1")
+        } else {
+            sp[0].clone()
+        }
+    }
+
+    pub fn add2<T: Into<String>>(&mut self, name: T) -> String {
+        let sa = SuffixArray::new(name.into());
+
+        if Self::is_should_be_searched(sa.get_text()) {
+            let subs = sa.get_substrs();
+            if self.patterns.is_empty() {
+                self.patterns = subs
+            } else {
+                self.patterns = self.patterns.intersection(&subs).cloned().collect();
+            }
+        } else {
+            let mut keep = HashSet::new();
+            for pattern in self.patterns.iter() {
+                if sa.search(pattern).is_none() {
+                    keep.insert(pattern.clone());
+                }
+            }
+            self.patterns = keep;
+        }
+
+        let mut v = self.patterns.iter().cloned().collect::<Vec<String>>();
+        v.sort_by(|a, b| a.len().cmp(&b.len()).then(a.cmp(&b)));
+        println!("sp: {:?}\n", v);
+        if v.is_empty() {
+            String::from("-1")
+        } else {
+            v[0].clone()
+        }
+    }
 }
 
 fn main() {
@@ -21,8 +99,8 @@ fn main() {
     let mut calc = Calculator::new();
     for _ in 0..n {
         let name = read_line::<String>();
-        assert!(name.len() <= 10);
-        println!("{}", name);
+        assert!(name.len() <= 11);
+        println!("{}", calc.add2(name));
     }
 }
 
@@ -30,10 +108,12 @@ fn main() {
 mod problem_d {
     #[test]
     fn sanity() {
-        let mut calc = super::Calculator::new(3);
-        calc.add(1);
-        assert_eq!(None, calc.get(1));
-        calc.add(3);
-        assert_eq!(Some(2), calc.get(3));
+        let mut calc = super::Calculator::new();
+        assert_eq!("_", calc.add2("mit_kotiki+"));
+        assert_eq!("_", calc.add2("sjtu_koty+"));
+        assert_eq!("k", calc.add2("itmo_first"));
+        assert_eq!("ot", calc.add2("msu_koshki"));
+        assert_eq!("kot", calc.add2("mipt_alot"));
+        assert_eq!("-1", calc.add2("spsu_kot"));
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use std::collections::HashSet;
 use std::fmt::Debug;
 use std::str::FromStr;
 
@@ -26,4 +27,110 @@ where
     let first = tokens[0].trim().parse::<F>().expect("Can't parse string");
     let second = tokens[1].trim().parse::<S>().expect("Can't parse string");
     return (first, second);
+}
+
+#[derive(Debug)]
+struct SuffixIndex {
+    suffix: String,
+    index: usize,
+}
+
+#[derive(Debug)]
+pub struct SuffixArray {
+    pub text: String,
+    suffix_array: Vec<SuffixIndex>,
+}
+
+impl SuffixArray {
+    pub fn get_text(&self) -> String {
+        return self.text.clone();
+    }
+
+    pub fn get_suffix_idx(&self) -> Vec<usize> {
+        self.suffix_array.iter().map(|x| x.index).collect()
+    }
+
+    pub fn get_suffix(&self, idx: usize) -> String {
+        self.suffix_array[idx].suffix.clone()
+    }
+
+    pub fn new<T: Into<String>>(text: T) -> Self {
+        let text = text.into();
+        let mut suffix_array = Vec::with_capacity(text.len());
+        for idx in 0..(text.len()) {
+            let suffix = text[idx..].to_string();
+            suffix_array.push(SuffixIndex {
+                suffix: suffix,
+                index: idx + 1,
+            });
+        }
+        suffix_array.sort_by(|a, b| a.suffix.cmp(&b.suffix));
+
+        Self { text, suffix_array }
+    }
+
+    pub fn search<T: Into<String>>(&self, pattern: T) -> Option<(usize, usize)> {
+        //  Return indices [s, r) such that the interval A[s..r]
+        //  represents all suffixes of S that start with the pattern P.
+        let p = pattern.into();
+        let mut left = 0;
+        let mut right = self.text.len();
+        while left < right {
+            let middle = (left + right) / 2;
+            if p > self.suffix_array[middle].suffix {
+                left = middle + 1
+            } else {
+                right = middle;
+            }
+        }
+        let start = left;
+        right = self.text.len();
+        while left < right {
+            let middle = (left + right) / 2;
+            let suffix = &self.suffix_array[middle].suffix;
+            if suffix.starts_with(&p) {
+                left = middle + 1;
+            } else {
+                right = middle;
+            }
+        }
+
+        if start != right {
+            return Some((start, right));
+        } else {
+            return None;
+        }
+    }
+
+    pub fn get_substrs(&self) -> HashSet<String> {
+        let mut set = HashSet::new();
+        for pair in self.suffix_array.iter() {
+            let suffix = &pair.suffix;
+            for end in 1..suffix.len() {
+                set.insert(suffix[0..end].to_string());
+            }
+        }
+        return set;
+    }
+}
+
+#[cfg(test)]
+mod suffix_array {
+    #[test]
+    fn sanity_banana() {
+        let sa = super::SuffixArray::new("banana");
+        assert_eq!(vec![6, 4, 2, 1, 5, 3], sa.get_suffix_idx());
+        assert_eq!(Some((0, 3)), sa.search("a"));
+        assert_eq!(Some((1, 3)), sa.search("an"));
+        assert_eq!(Some((2, 3)), sa.search("anan"));
+        assert_eq!(None, sa.search("ananas"));
+        assert_eq!(None, sa.search("apple"));
+        assert_eq!(None, sa.search("c"));
+    }
+    #[test]
+    fn sanity_abracadabra() {
+        let sa = super::SuffixArray::new("abracadabra");
+        assert_eq!(vec![11, 8, 1, 4, 6, 9, 2, 5, 7, 10, 3], sa.get_suffix_idx());
+        assert!(sa.get_substrs().iter().all(|s| sa.search(s).is_some()));
+    }
 }
